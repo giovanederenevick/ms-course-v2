8919590183a4776df52d06c8d284d10c356d8e5a
Giovane Derenevick Filho <giovanedfilho@gmail.com>
2021-08-31 20:47:04 -0300

    Configuring Eureka server and clients

--- hr-payroll/src/main/java/br/com/giovanederenevick/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/br/com/giovanederenevick/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package br.com.giovanederenevick.hrpayroll.resources;
 
 import br.com.giovanederenevick.hrpayroll.entities.Payment;
 import br.com.giovanederenevick.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,14 @@ public class PaymentResource {
     @Autowired
     private PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         Payment payment = service.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        Payment payment = new Payment("Brann", 400.0, days);
         return ResponseEntity.ok(payment);
     }
 }
