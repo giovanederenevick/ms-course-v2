f95d126e8e7daaaf51a60f7df5d4ef466f9c2f0d
Giovane Derenevick Filho <giovanedfilho@gmail.com>
2021-08-31 20:32:42 -0300

    Feign client and Ribbon load balancing

--- hr-payroll/src/main/java/br/com/giovanederenevick/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/giovanederenevick/hrpayroll/services/PaymentService.java
@@ -1,12 +1,26 @@
 package br.com.giovanederenevick.hrpayroll.services;
 
 import br.com.giovanederenevick.hrpayroll.entities.Payment;
+import br.com.giovanederenevick.hrpayroll.entities.Worker;
+import br.com.giovanederenevick.hrpayroll.feignclients.WorkerFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Autowired
+    private WorkerFeignClient workerFeignClient;
+
     public Payment getPayment(long workerId, int days) {
-        return new Payment("Bob", 200.0, days);
+
+        Worker worker = workerFeignClient.findById(workerId).getBody();
+
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
