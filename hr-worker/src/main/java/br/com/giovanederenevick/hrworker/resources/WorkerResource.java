d9f316ff7460c8aa4f45473f806f250368835533
Giovane Derenevick Filho <giovanedfilho@gmail.com>
2021-08-31 21:13:07 -0300

    Configuring hr-config-server and clients

--- hr-worker/src/main/java/br/com/giovanederenevick/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/giovanederenevick/hrworker/resources/WorkerResource.java
@@ -3,6 +3,8 @@ package br.com.giovanederenevick.hrworker.resources;
 import br.com.giovanederenevick.hrworker.entities.Worker;
 import br.com.giovanederenevick.hrworker.repositories.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
