bf2c5caef01374816983bc6308efa4f1a781c8c8
Giovane Derenevick Filho <giovanedfilho@gmail.com>
2021-08-31 21:15:30 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/br/com/giovanederenevick/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.giovanederenevick.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
